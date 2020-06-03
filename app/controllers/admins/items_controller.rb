37a4cb82bbff4f813c5975443576e9904a4c3f4a
花田　結衣 <yui@hanadayuinoMBP.lan>
2020-06-03 11:40:29 +0900

    update items/new.html.erb

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -8,5 +8,13 @@ class Admin::ItemsController < ApplicationController
 	end
 
 	def create
+		@item = Item.new(item_params)
+		if @item.save
+			flash[:notice] = "商品登録が完了しました"
+			redirect_to admins_items_path
+		else
+			@item = Item.new
+			render :new
+		end
 	end
 end

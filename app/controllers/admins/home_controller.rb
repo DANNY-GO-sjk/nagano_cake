9a0b2dba3e15026b20e8db8a28d5c62cc4279986
cebasypia <49581588+cebasypia@users.noreply.github.com>
2020-06-03 14:26:13 +0900

    Orders => Order
    タイポを修正

--- app/controllers/admins/home_controller.rb
+++ app/controllers/admins/home_controller.rb
@@ -1,5 +1,5 @@
 class Admins::HomeController < ApplicationController
 	def index
-		@orders = Orders.all # １日の注文数ですが、未実装です。一時的に全件取得しています。
+		@orders = Order.all # １日の注文数ですが、未実装です。一時的に全件取得しています。
 	end
 end

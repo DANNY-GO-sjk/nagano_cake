bd3afd464842598d2c6e2554c6f577b243d41a8a
花田　結衣 <yui@hanadayuinoMBP.lan>
2020-06-03 17:39:40 +0900

    update genre controller

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -3,4 +3,19 @@ class Admins::GenresController < ApplicationController
 		@genres = Genres.all
 		@genre = Genre.new
 	end
+	def create
+		@genre = Genre.new(genre_params)
+		if @genre.save
+			flash[:notice] = "ジャンル登録が完了しました"
+			redirect_to admins_genres_path
+		else
+			@genres = Genres.all
+			render :index
+		end
+	end
+
+	private
+	def genre_params
+		params.require(:genre).permit(:name, :is_valid)
+	end
 end

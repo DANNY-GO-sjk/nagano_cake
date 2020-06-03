93a839a385cc2e87fd41519ffca8ef3bec000e1a
vagrant <vagrant@localhost.localdomain>
2020-06-03 01:57:51 +0000

    fix application_controller sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(resource)
-    items_path
+    users_path(resource)
   end
 
   def after_sign_out_path_for(resource)
@@ -16,6 +16,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_yomi, :first_name_yomi, :postcode, :address, :phone_number,:email])
-
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name,:email,:password])
   end
 end

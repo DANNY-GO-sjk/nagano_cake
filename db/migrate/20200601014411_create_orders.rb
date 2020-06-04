3c32348836e0663f3a6fc800b69503b660122666
vagrant <cebasypia.tskm8@gmail.com>
2020-06-04 07:10:03 +0000

    Add default to progress

--- db/migrate/20200601014411_create_orders.rb
+++ db/migrate/20200601014411_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
       t.references  :user,           null: false, foreign_key: true
-      t.integer     :progress,       null: false
+      t.integer     :progress,       null: false, default: 0
       t.integer     :shipping_price, null: false, default: 800
       t.integer     :total_price,    null: false
       t.integer     :payment_method, null: false, default: 0

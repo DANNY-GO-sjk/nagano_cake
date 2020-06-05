15e860d6912d4c491fe6e0f5fbc2f669fa782478
cebasypia <49581588+cebasypia@users.noreply.github.com>
2020-06-05 15:55:39 +0900

    インデントの修正
    インデントを揃えました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,22 @@
 class ItemsController < ApplicationController
   def index
-     @genres = Genre.where(is_valid:true)
-     # is_validがマッチするレコードを全て取得
-
-     if params[:genre_id]
+    # is_validがマッチするレコードを全て取得
+    @genres = Genre.where(is_valid:true)
+    
+    if params[:genre_id]
       # genreのデータベースのテーブルから一致するidを取得
       @genre = Genre.find(params[:genre_id])
-
+      
       #genre_idと紐ずく商品を取得(ジャンル)(8個取得)
       @items = @genre.items.all.page(params[:page]).per(8)
     else
       # 商品全てを取得
       @items =Item.page(params[:page]).per(8)
-      end
+    end
   end
 
   def show
-     @genres = Genre.where(is_valid: true)
-     @item = Item.find(params[:id])
+    @genres = Genre.where(is_valid: true)
+    @item = Item.find(params[:id])
   end
 end

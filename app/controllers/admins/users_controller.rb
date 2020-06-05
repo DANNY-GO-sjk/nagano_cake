a6465306bf08c8144d39bc9698fbf117bc9578c2
vagrant <cebasypia.tskm8@gmail.com>
2020-06-05 02:39:39 +0000

    Add admins/users#show

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,42 +1,39 @@
 class Admins::UsersController < ApplicationController
-	def index
-		@users = User.all
-	end
+  def index
+    @users = User.all
+  end
 
-	def show
-	   @user = current_user
-	end
+  def show
+    @user = User.find(params[:id])
+  end
 
-	def edit
-		@user = User.find(current_user.id)
-	end
+  def edit
+    @user = User.find(current_user.id)
+  end
 
-	def update
-		@user = User.find(current_user.id)
-		if @user.update(user_params)
-			redirect_to users_path(@user)
-		else
-			render 'edit'
-		end
-	end
+  def update
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      redirect_to users_path(@user)
+    else
+      render 'edit'
+    end
+  end
 
-	def edit_withdraw
-		@user = User.find(current_user.id)
-	end
+  def edit_withdraw
+    @user = User.find(current_user.id)
+  end
 
-	def withdraw
-		@user = User.find(current_user.id)
-		@user.update(is_valid: true) #会員ステータスの退会を有効にする
-		@end_user.destroy
-		redirect_to root_path
-	end
+  def withdraw
+    @user = User.find(current_user.id)
+    @user.update(is_valid: true) # 会員ステータスの退会を有効にする
+    @end_user.destroy
+    redirect_to root_path
+  end
 
-	private
-	def user_params
-		params.require(:user).permit(:family_name, :first_name, :family_name_yomi, :first_name_yomi, :postcode, :address, :phone_number, :is_valid)
-	end
+  private
 
-
+  def user_params
+    params.require(:user).permit(:family_name, :first_name, :family_name_yomi, :first_name_yomi, :postcode, :address, :phone_number, :is_valid)
+  end
 end
-
-

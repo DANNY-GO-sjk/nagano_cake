45cb96304fe463c19c21d2723a8db452494d5657
花田　結衣 <yui@hanadayuinoMBP.lan>
2020-06-05 15:39:08 +0900

    create-genre-edit

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -15,6 +15,20 @@ class Admins::GenresController < ApplicationController
     end
   end
 
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      flash[:notice] = "編集を保存しました"
+      redirect_to admins_genres_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def genre_params

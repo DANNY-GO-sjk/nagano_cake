704be3305257d17d4a42d84ef5150f67667d12b2
Numazakikouya <vagrant@localhost.localdomain>
2020-06-05 17:10:43 +0000

    create admins order detail

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -2,4 +2,15 @@ class Admins::OrdersController < ApplicationController
 	def index
 		@orders = Order.all
 	end
+	def show
+		@order = Order.find(params[:id])
+	end
+	def update
+		
+	end
+
+private 
+	def order_params
+		params.require(:order).permit(:user_id, :postcode, :payment_method, :progress, :address)
+	end
 end

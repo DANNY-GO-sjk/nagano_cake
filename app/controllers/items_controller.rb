0db139a2d4fb91c69a518652e9341e37caafa1bc
vagrant <cebasypia.tskm8@gmail.com>
2020-06-07 07:07:27 +0000

    Fix to select cart_item :how_many from items#show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,21 @@
 class ItemsController < ApplicationController
   def index
     # is_validがマッチするレコードを全て取得
-    @genres = Genre.where(is_valid:true)
+    @genres = Genre.where(is_valid: true)
     if params[:genre_id]
       # genreのデータベースのテーブルから一致するidを取得
       @genre = Genre.find(params[:genre_id])
-      #genre_idと紐ずく商品を取得(ジャンル)(8個取得)
+      # genre_idと紐ずく商品を取得(ジャンル)(8個取得)
       @items = @genre.items.all.page(params[:page]).per(8)
     else
       # 商品全てを取得
-      @items =Item.page(params[:page]).per(8)
+      @items = Item.page(params[:page]).per(8)
     end
   end
 
   def show
     @genres = Genre.where(is_valid: true)
     @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
 end

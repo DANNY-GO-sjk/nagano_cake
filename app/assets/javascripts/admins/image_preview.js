991dc2461903a405f51244a74ca68f3aac82c831
vagrant <cebasypia.tskm8@gmail.com>
2020-06-11 00:58:38 +0000

    Remove dead code

--- app/assets/javascripts/admins/image_preview.js
+++ app/assets/javascripts/admins/image_preview.js
@@ -1,16 +1,5 @@
 $(document).on('turbolinks:load', function () {
   $(function () {
-    // 画像をプレビュー表示させる.prev-contentを作成
-    function buildHTML(image) {
-      var html =
-        `
-        <div class="prev-content">
-          <img src="${image}", alt="preview" class="prev-image">
-        </div>
-        `
-      return html;
-    }
-
     // 画像が選択された時に発火します
     $(document).on('change', '#item_image', function () {
       // .file_filedからデータを取得して変数fileに代入します
@@ -23,18 +12,8 @@ $(document).on('turbolinks:load', function () {
       reader.onload = function () {
         // 読み込んだファイルの内容を取得して変数imageに代入します
         const image = this.result;
-        // プレビュー画像がなければ処理を実行します
-        // if ($('.prev-content').length == 0) {
-        //   // 読み込んだ画像ファイルをbuildHTMLに渡します
-        //   var html = buildHTML(image)
-        //   // 作成した.prev-contentをiconの代わりに表示させます
-        //   $('.prev-contents').prepend(html);
-        //   // 画像が表示されるのでiconを隠します
-        //   $('.photo-icon').hide();
-        // } else {
         // もし既に画像がプレビューされていれば画像データのみを入れ替えます
         $('.prev-content .prev-image').attr({ src: image });
-        // }
       }
     });
   });

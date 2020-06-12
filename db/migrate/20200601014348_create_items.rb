a9a3ef66b82cbcae874e28f6051fa8585e40f2a5
vagrant <cebasypia.tskm8@gmail.com>
2020-06-12 06:33:22 +0000

    Add recommending item Fix preview

--- db/migrate/20200601014348_create_items.rb
+++ db/migrate/20200601014348_create_items.rb
@@ -4,9 +4,10 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.references  :genre, foreign_key: true
       t.string      :name, null: false
       t.integer     :price, null: false
-      t.string      :image_id # null: false
-      t.text        :explanation # null: false
+      t.string      :image_id, null: false
+      t.text        :explanation, null: false
       t.boolean     :is_valid, null: false, default: true
+      t.datetime    :recommended
       t.timestamps
     end
   end

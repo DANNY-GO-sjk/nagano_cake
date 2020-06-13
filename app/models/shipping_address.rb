c3ce702d984d61a25e4b81bb4d2d37d868c9fcb8
花田　結衣 <yui@hanadayuinoMBP.lan>
2020-06-13 09:57:19 +0900

    edit-shipping-address-model

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,4 +1,9 @@
 class ShippingAddress < ApplicationRecord
+  belongs_to :user
+
+  validates :postcode, presence: true,　numericality: { only_integer: true, :equal_to 7 }
+  validates :address, presence: true
+  validates :receiver, presence: true
   def full_address
     "#{postcode} #{address} #{receiver}"
   end
